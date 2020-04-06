76af4b9c00cddff00ff55e51966bd832950fab97
code01 <idrcorner@gmailcom>
2020-04-06 17:04:29 +0700

    hapus logoo

---
+++ build/precache-manifest.6969ae6ce2448b5f3691ac499430a166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c392320edca104a28c7ee48102c815",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc372084f938fd47c027",
+    "url": "/static/css/main.65b3bdef.chunk.css"
+  },
+  {
+    "revision": "f66ff659611da75dc325",
+    "url": "/static/js/2.60015896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.60015896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc372084f938fd47c027",
+    "url": "/static/js/main.0da479b4.chunk.js"
+  },
+  {
+    "revision": "5bbafceaa101c52dd456",
+    "url": "/static/js/runtime-main.c5ae66f2.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0033bf5e614584acc6293d892375fc4d.js"
+  "/precache-manifest.6969ae6ce2448b5f3691ac499430a166.js"
 );
 
 self.addEventListener('message', (event) => {
